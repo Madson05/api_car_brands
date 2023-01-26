5029663650d2b77a2869be45fd056d4656098e53
madson <madsonsilva2005@gmail.com>
2023-01-25 22:18:00 -0300

    refatorada as rotas moreCars e lessCars e implementada nva rota para ordenação de marcas com mais modelos de acordo com quantidade passada por parametro na URL

--- src/routers/routerCars.js
+++ src/routers/routerCars.js
@@ -14,30 +14,55 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/moreCars", async (req, res, next)=> {
-  const data = JSON.parse(await readFile(fileName));
-  let brandWithMoreModels = ["", 0];
+router.get("/moreCars", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(fileName));
+    let numberMax = Math.max(...data.map((car) => car.models.length));
 
-  data.map((brand) => {
-    if(brand.models.length > brandWithMoreModels[1]){
-      brandWithMoreModels[0] = brand.brand;
-      brandWithMoreModels[1] = brand.models.length;
-    } 
-  })
-  res.send(brandWithMoreModels[0])
+    let bigBrands = data
+      .filter((car) => car.models.length === numberMax)
+      .map((car) => car.brand);
+    res.send(bigBrands);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/lessCars", async (req, res, next)=> {
-  const data = JSON.parse(await readFile(fileName));
-  let brandWithLessModels = ["", data[0].models.length];
+router.get("/lessCars", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(fileName));
+    let numberMin = Math.min(...data.map((car) => car.models.length));
 
-  data.map((brand) => {
-    if(brand.models.length < brandWithLessModels[1]){
-      brandWithLessModels[0] = brand.brand;
-      brandWithLessModels[1] = brand.models.length;
-    } 
-  })
-  res.send(brandWithLessModels[0])
+    let smallBrands = data
+      .filter((car) => car.models.length === numberMin)
+      .map((car) => car.brand);
+
+    res.send(smallBrands);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/moreCars/:number", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(fileName));
+    const lista = data.sort((a, b) => {
+      if (a.models.length > b.models.length) {
+        return -1;
+      } else if (a.models.length == b.models.length) {
+        if (a.brand < b.brand) {
+          return -1;
+        } else {
+          return 1;
+        }
+      } else return 0;
+    }).map((car) => `${car.brand} - ${car.models.length}`)
+    
+
+    res.send(lista.slice(0,5));
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.use((error, req, res, next) => {
